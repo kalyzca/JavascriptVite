1f39c9cc20dc39a0a4ac5133f9e9cc02e9d74a2d
Kaly <alcantarakaly@gmail.com>
2025-05-04 21:49:18 -0500

    Add-configuración inicial

---
+++ src/bases/const_let.js
@@ -0,0 +1,17 @@
+// variables y constantes
+const nombre = 'Kaly';
+let apellido_paterno = 'Cristóbal';
+let edad = 23;
+
+edad=41;
+
+console.log(nombre,apellido_paterno,edad);
+
+console.log(`${nombre} ${apellido_paterno} tiene ${edad} años.`);
+
+if (true) {
+    let apellido_paterno="Perez";
+    console.log(apellido_paterno);
+}
+
+console.log(apellido_paterno);
